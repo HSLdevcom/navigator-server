12dc99b74459d615c83b7fc3ea78dc3a4c5bf6f5
Tuukka Hastrup <Tuukka.Hastrup@iki.fi>
2015-06-24 16:55:26 +0200

    add new helmi lines; add checks for unknown lines

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import sys
+import re
 import csv
 
 dest_csv = csv.reader(open('dest.skv', 'r'), delimiter=';')
@@ -11,14 +12,17 @@ dep_times = {}
 def jore_code(line):
   if line in ('506', '512K', '550', '550B', '552', '554', '554K'):
     return '2'+line
-  elif line in ('415', '451', '519', '519A', '520', '615', '615T', '615TK', '615V', '615VK', '620'):
+  elif line in ('415', '451', '519', '519A', '520', '560', '615', '615T', '615TK', '615V', '615VK', '620', '665', '665A'):
     return '4'+line
   elif line in ('61', '61V'):
     return '40'+line
   elif line in ('1', '1A', '2', '2X', '3', '3X', '4', '4T', '5', '6', '6T', '6X', '7A', '7B', '8', '9', '9X'):
     return '100'+line
-  else:
+  elif len(re.match(r'\d*', line).group(0)) == 2: # default case for 2 digits:
     return '10'+line
+  else:
+    print >>sys.stderr, "JORE code not known for line '%s'" % line
+    return None
 
 for line in dest_csv:
   dest[line[3]] = jore_code(line[4].split(' ')[0])
